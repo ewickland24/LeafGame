f1a673ded13dab75eecdd2a6e2e2104eef5d57ba
HungryDoor <noahmays@letu.edu>
2026-04-18 09:19:14 -0700

    Squashed commit of the following:
    commit bfddf15cb365eb80dbcde3b4fc54c6181f9660f2
    Merge: 7ae1b18 5c4e79e
    Author: HungryDoor <noahmays@letu.edu>
    Date:   Sat Apr 18 09:14:33 2026 -0700
    
        Merge branch 'mechanics' of https://github.com/ewickland24/LeafGame into mechanics
    
    commit 7ae1b18faf0fbac13a2e0abd638f742bfea4888c
    Author: HungryDoor <noahmays@letu.edu>
    Date:   Sat Apr 18 09:14:29 2026 -0700
    
        Leaf flow be go be go
    
        he do be flowin in the "wind?" i think
    
    commit 5c4e79eaf342a50fefb26f0dfe98b2bcdd869ac1
    Author: HungryDoor <166181740+HungryDoor@users.noreply.github.com>
    Date:   Fri Apr 17 22:42:13 2026 -0500
    
        Create level scenes and add backgrounds

---
+++ leaf-game/scripts/da_leaf_character.gd
@@ -0,0 +1,80 @@
+extends CharacterBody2D
+
+var gravityTax := 2
+var rotation_speed := 2.2
+var Y_MAX_SPEED := 800
+var X_MAX_SPEED := 1200
+var magnitude := 0.0
+var loft : float = 0.0
+var leafFlow : float = 0.0
+var inputSettings : int = 1
+var gameOver : bool = false
+
+func _physics_process(delta):
+	
+	# swap from keys to mouse controlls
+	if Input.is_action_just_pressed("ui_accept"):
+		inputSettings = inputSettings * -1
+	
+	# player rotation control 
+	if (not gameOver):
+		if (sign(inputSettings) == -1):
+			look_at(get_global_mouse_position())
+		else:
+			if Input.is_action_pressed("left"):
+				rotation -= rotation_speed * delta
+			elif Input.is_action_pressed("right"):
+				rotation += rotation_speed * delta
+	
+	# magnitude of speed based on facing
+	magnitude += abs(rotation)
+	magnitude = abs(0.99 * magnitude) * sign(rotation)
+	magnitude = max(-Y_MAX_SPEED, min(Y_MAX_SPEED, magnitude))
+	
+	# allowing players to use breeze
+	if (Input.is_action_pressed("breeze")):
+		velocity.x += 3 * cos(rotation)
+		velocity.y -= 10.0 * abs(sin(rotation))
+		loft -= 0.05
+	
+	if (Input.is_action_just_pressed("upDraft")):
+		loft = 0.0
+		magnitude = 0.0
+		velocity.y = min(-600, velocity.y)
+		velocity.y -= 600
+	
+	#make the player travel with less momentum the longer they look up
+	if (sign(magnitude) == -1):
+		magnitude += loft
+		loft += 0.03
+	else:
+		if(loft > 1):
+			loft = loft * 0.99
+		else:
+			loft = 0.0
+	
+	# calculate x velocity
+	if (sign(rotation) == 1):
+		leafFlow = leafFlow + 0.1 * magnitude
+		velocity.x += 0.01 * leafFlow * cos(rotation) 
+	else:
+		leafFlow = 0.1 * leafFlow
+		velocity.x += sin(rotation) * 5
+	
+	# set max speeds
+	velocity.x = max(-X_MAX_SPEED/5.0, min(X_MAX_SPEED, velocity.x))
+	velocity.y += magnitude + gravityTax * delta
+	velocity.y = max(-Y_MAX_SPEED, min(Y_MAX_SPEED, velocity.y))
+	
+	if (rotation == 0.0):
+		velocity.y = 0
+	
+	# ending game run
+	if (is_on_floor()):
+		gameOver = true
+		velocity.y = 0
+		velocity.x = 0
+	
+	
+	# --- Move ---
+	move_and_slide()
