8625b665499e5bd5332cef7b49a11cf29ee40f83
HungryDoor <166181740+HungryDoor@users.noreply.github.com>
2026-04-18 13:47:09 -0500

    pause menu done; start level selection menu (#6)

--- leaf-game/scenes/pause_menu.gd
+++ leaf-game/scenes/pause_menu.gd
@@ -2,6 +2,9 @@ extends Control
 
 
 # Called when the node enters the scene tree for the first time.
+func _ready():
+	$AnimationPlayer.play("RESET")
+	
 func resume():
 	get_tree().paused = false
 	$AnimationPlayer.play_backwards("blur")
@@ -11,16 +14,20 @@ func pause():
 	$AnimationPlayer.play("blur")
 	
 func test_escape():
-	if Input.is_action_just_pressed("esc") and get_tree().paused == false:
+	if Input.is_action_just_pressed("escape") and !get_tree().paused:
 		pause()
-	if Input.is_action_just_pressed("esc") and get_tree().paused == true:
+	elif Input.is_action_just_pressed("escape") and get_tree().paused:
 		resume() 
 
 func _on_resume_button_pressed() -> void:
 	resume()
 
 func _on_restart_button_pressed() -> void:
+	resume()
 	get_tree().reload_current_scene()
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
+
+func _process(delta: float) -> void:
+	test_escape()
