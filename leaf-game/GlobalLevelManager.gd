06ff79548ab5113f0b4196d4808a37b1d7a5034b
HungryDoor <166181740+HungryDoor@users.noreply.github.com>
2026-04-18 13:46:31 -0500

    pause menu done; start level selection menu

---
+++ leaf-game/GlobalLevelManager.gd
@@ -0,0 +1,22 @@
+extends Node
+
+var current_level: int = 1
+var level_unlocked: int = 1
+var max_level: int = 4
+
+func unlock_level(level_to_unlock: int) -> void:
+	if level_to_unlock > level_unlocked:
+		level_unlocked = level_to_unlock
+
+func load_level(level_to_load: int) -> String:
+	if level_to_load > max_level:
+		return ""
+	return str("res://scenes/level", level_to_load, ".tscn")
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
