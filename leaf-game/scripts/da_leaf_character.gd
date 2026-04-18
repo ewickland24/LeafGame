eae3fbc1f6bd890711b7c8da1098fcb4cf1df5e6
HungryDoor <noahmays@letu.edu>
2026-04-18 15:09:12 -0700

    difficulty stuffs be there
    huzzah

--- leaf-game/scripts/da_leaf_character.gd
+++ leaf-game/scripts/da_leaf_character.gd
@@ -17,9 +17,12 @@ var draftNum : int = 3
 var totalBreeze = 10.0
 
 var gravityTax := 2
-var rotation_speed := 2.2
 var Y_MAX_SPEED := 800
 var X_MAX_SPEED := 1200
+var loftTax := 0.03
+var maxBreeze := 0.0
+
+var rotation_speed := 2.2
 var magnitude := 0.0
 var loft : float = 0.0
 var leafFlow : float = 0.0
@@ -62,8 +65,8 @@ func _physics_process(delta):
 	# allowing players to use breeze
 	if (Input.is_action_pressed("breeze") && totalBreeze > 0 && rotation != 0.0):
 		totalBreeze -= 0.1
-		breezeBox.scale.x = totalBreeze
-		breezeBox.position.x -= 6
+		breezeBox.scale.x = 10 * (totalBreeze/maxBreeze)
+		breezeBox.position.x -= 3.3
 		velocity.x += 3 * cos(rotation)
 		velocity.y -= 10.0 * abs(sin(rotation))
 		loft -= 0.05
@@ -75,12 +78,11 @@ func _physics_process(delta):
 		magnitude = 0.0
 		velocity.y = min(-400, velocity.y)
 		velocity.y -= 400
-		loadingLeaf(0)
 	
 	#make the player travel with less momentum the longer they look up
 	if (sign(magnitude) == -1):
 		magnitude += loft
-		loft += 0.03
+		loft += loftTax
 	else:
 		if(loft > 1):
 			loft = loft * 0.99
@@ -116,3 +118,49 @@ func _physics_process(delta):
 func loadingLeaf(numba):
 	enviornment = numba
 	leafAnimate.play("leaf"+str(numba))
+	
+	if(enviornment == 0):
+		draftNum = 3
+		totalBreeze = 20.0
+		maxBreeze = 20.0
+		gravityTax = 2
+		Y_MAX_SPEED = 800
+		X_MAX_SPEED = 1200
+		loftTax = 0.03
+	elif(enviornment == 1):
+		draftNum = 2
+		totalBreeze = 15.0
+		maxBreeze = 15.0
+		gravityTax = 2
+		Y_MAX_SPEED = 800
+		X_MAX_SPEED = 1200
+		loftTax = 0.04
+		daDrafts[2].visible = false
+	elif(enviornment == 2):
+		draftNum = 1
+		totalBreeze = 10.0
+		maxBreeze = 10.0
+		gravityTax = 2
+		Y_MAX_SPEED = 800
+		X_MAX_SPEED = 1200
+		loftTax = 0.05
+		daDrafts[2].visible = false
+		daDrafts[1].visible = false
+	elif(enviornment == 3):
+		draftNum = 0
+		totalBreeze = 5.0
+		maxBreeze = 5.0
+		gravityTax = 2
+		Y_MAX_SPEED = 800
+		X_MAX_SPEED = 1200
+		loftTax = 0.06
+		daDrafts[2].visible = false
+		daDrafts[1].visible = false
+		daDrafts[0].visible = false
+	else:
+		draftNum = 3
+		totalBreeze = 10.0
+		gravityTax = 2
+		Y_MAX_SPEED = 10000
+		X_MAX_SPEED = 10000
+		loftTax = 0.03
